7d8a9ac80f95ab12c0a7db3aee446a682cf4587d
Yeoh Ee Peng <ee.peng.yeoh@intel.com>
2018-10-30 16:53:53 +0800

    scripts/test-case-mgmt: store test result & log into git repository
    These scripts were developed as an alternative testcase management tool
    to Testopia. Using these scripts, user can store test result & log from
    OEQA automated testcase execution.
    
    These scripts will store test result & log in GIT repository.
    To use these scripts, first source oe environment, then run the entry
    point script to look for help.
        $ test-case-mgmt
    
    To store test result for OEQA automated testcase, execute the below
        $ test-case-mgmt store <source_dir> <git_branch>
    
    Signed-off-by: Yeoh Ee Peng <ee.peng.yeoh@intel.com>

---
+++ scripts/lib/testcasemgmt/gitstore.py
@@ -0,0 +1,117 @@
+# test case management tool - store test result & log to git repository
+#
+# Copyright (c) 2018, Intel Corporation.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+import tempfile
+import os
+import subprocess
+import shutil
+import scriptpath
+scriptpath.add_bitbake_lib_path()
+scriptpath.add_oe_lib_path()
+from oeqa.utils.git import GitRepo, GitError
+from oe.path import copytree
+
+class GitStore(object):
+
+    def _check_if_dir_contain_sub_dir(self, dir, sub_dir):
+        if os.path.exists(os.path.join(dir, sub_dir)):
+            return True
+        else:
+            return False
+
+    def _git_init(self, git_dir):
+        try:
+            repo = GitRepo(git_dir, is_topdir=True)
+        except GitError:
+            print("Non-empty directory that is not a Git repository "
+                   "at {}\nPlease specify an existing Git repository, "
+                   "an empty directory or a non-existing directory "
+                   "path.".format(git_dir))
+        return repo
+
+    def _run_git_cmd(self, repo, cmd):
+        try:
+            output = repo.run_cmd(cmd)
+            return True, output
+        except GitError:
+            return False, None
+
+    def _check_if_git_dir_exist(self, git_dir):
+        if not os.path.exists('%s/.git' % git_dir):
+            return False
+        return True
+
+    def _checkout_git_dir(self, git_dir, git_branch):
+        repo = self._git_init(git_dir)
+        cmd = 'checkout %s' % git_branch
+        return self._run_git_cmd(repo, cmd)
+
+    def _create_temporary_workspace_dir(self):
+        return tempfile.mkdtemp(prefix='testresultlog.')
+
+    def _remove_temporary_workspace_dir(self, workspace_dir):
+        return subprocess.run(["rm", "-rf",  workspace_dir])
+
+    def _make_directories(self, logger, target_dir):
+        logger.debug('Creating directories: %s' % target_dir)
+        bb.utils.mkdirhier(target_dir)
+
+    def _copy_files_from_source_to_destination_dir(self, logger, source_dir, destination_dir):
+        if os.path.exists(source_dir) and os.path.exists(destination_dir):
+            logger.debug('Copying test result & log from %s to %s' % (source_dir, destination_dir))
+            copytree(source_dir, destination_dir)
+
+    def _push_testresult_files_to_git_repo(self, logger, file_dir, git_dir, git_branch, git_sub_dir):
+        logger.debug('Storing test result & log inside git repository (%s) and branch (%s)'
+                     % (git_dir, git_branch))
+        commit_msg_subject = 'Store %s from {hostname}' % os.path.join(git_dir, git_sub_dir)
+        commit_msg_body = 'git dir: %s\nsub dir list: %s\nhostname: {hostname}' % (git_dir, git_sub_dir)
+        return subprocess.run(["oe-git-archive",
+                               file_dir,
+                               "-g", git_dir,
+                               "-b", git_branch,
+                               "--commit-msg-subject", commit_msg_subject,
+                               "--commit-msg-body", commit_msg_body])
+
+    def _store_test_result_from_empty_git(self, logger, source_dir, git_branch, git_dir, git_sub_dir):
+        workspace_dir = self._create_temporary_workspace_dir()
+        full_workspace_dir = os.path.join(workspace_dir, git_sub_dir)
+        self._make_directories(logger, full_workspace_dir)
+        self._copy_files_from_source_to_destination_dir(logger, source_dir, full_workspace_dir)
+        self._push_testresult_files_to_git_repo(logger, workspace_dir, git_dir, git_branch, git_sub_dir)
+        self._remove_temporary_workspace_dir(workspace_dir)
+
+    def _store_test_result_from_existing_git(self, logger, source_dir, git_branch, git_dir, git_sub_dir):
+        self._make_directories(logger, os.path.join(git_dir, git_sub_dir))
+        self._copy_files_from_source_to_destination_dir(logger, source_dir, os.path.join(git_dir, git_sub_dir))
+        self._push_testresult_files_to_git_repo(logger, git_dir, git_dir, git_branch, git_sub_dir)
+
+    def store_test_result(self, logger, source_dir, git_branch, git_dir, git_sub_dir, overwrite_result):
+        logger.debug('Initializing store the test result & log')
+        if self._check_if_git_dir_exist(git_dir) and self._checkout_git_dir(git_dir, git_branch):
+            logger.debug('Found destination git directory and git branch: %s %s' % (git_dir, git_branch))
+            if self._check_if_dir_contain_sub_dir(git_dir, git_sub_dir):
+                logger.debug('Found existing sub (%s) directory inside: %s' % (git_sub_dir, git_dir))
+                if overwrite_result:
+                    logger.debug('Overwriting existing testresult inside: %s' % (os.path.join(git_dir, git_sub_dir)))
+                    shutil.rmtree(os.path.join(git_dir, git_sub_dir))
+                    self._store_test_result_from_existing_git(logger, source_dir, git_branch, git_dir, git_sub_dir)
+                else:
+                    logger.debug('Skipped storing test result & log as it already exist. '
+                                 'Specify overwrite if you wish to delete existing testresult and store again.')
+            else:
+                logger.debug('Could not find existing sub (%s) directories inside: %s' % (git_sub_dir, git_dir))
+                self._store_test_result_from_existing_git(logger, source_dir, git_branch, git_dir, git_sub_dir)
+        else:
+            logger.debug('Could not find destination git directory (%s) or git branch (%s)' % (git_dir, git_branch))
+            self._store_test_result_from_empty_git(logger, source_dir, git_branch, git_dir, git_sub_dir)
---
+++ scripts/lib/testcasemgmt/store.py
@@ -0,0 +1,40 @@
+# test case management tool - store test result
+#
+# Copyright (c) 2018, Intel Corporation.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+from testcasemgmt.gitstore import GitStore
+
+def store(args, logger):
+    gitstore = GitStore()
+    gitstore.store_test_result(logger, args.source_dir, args.git_branch, args.git_dir, args.git_sub_dir,
+                               args.overwrite_result)
+    return 0
+
+def register_commands(subparsers):
+    """Register subcommands from this plugin"""
+    parser_build = subparsers.add_parser('store', help='Store test result files into git repository.',
+                                         description='Store test result files into git repository.',
+                                         group='store')
+    parser_build.set_defaults(func=store)
+    parser_build.add_argument('source_dir',
+                              help='Source directory that contain the test result files to be stored.')
+    parser_build.add_argument('git_branch', help='Git branch used to store the test result files.')
+    parser_build.add_argument('-d', '--git_dir', default='',
+                              help='(Optional) Destination directory to be used/created as git repository '
+                                   'to store the test result files from the source directory. '
+                                   'Default location for destination directory will be <top_dir>/testresults.git.')
+    parser_build.add_argument('-s', '--git_sub_dir', default='',
+                              help='(Optional) Additional sub directory to be used/created under the destination '
+                                   'git repository, this sub directory will be used to hold the test result files. '
+                                   'Use sub directory if need a custom directory to hold test files.')
+    parser_build.add_argument('-o', '--overwrite_result', action='store_true',
+                              help='(Optional) To overwrite existing testresult file with new file provided.')
